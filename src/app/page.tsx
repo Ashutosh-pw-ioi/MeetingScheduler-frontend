fc12d56dd7d1c1fd418eb1f6376eb883fef9fced
nishb <nishbcodes@gmail.com>
2025-07-21 13:08:33 +0530

    Routes Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <h1 className="text-2xl font-bold">Select Your Role</h1>
       <button
-        onClick={() => router.push("/auth/login/student")}
+        onClick={() => router.push("/auth/login/interviewee")}
         className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer"
       >
         Interviewee
       </button>
       <button
-        onClick={() => router.push("/auth/login/teacher")}
+        onClick={() => router.push("/auth/login/interviewer")}
         className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 cursor-pointer"
       >
         Interviewer
