426cba3f55964b4b8f1ae0631b2760de4d304351
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-07-23 19:57:08 +0530

    Meeting api linked

---
+++ src/types/meeting.ts
@@ -0,0 +1,17 @@
+export interface Meeting {
+  id?: string | number;
+  student_name: string;
+  student_email: string;
+  student_phone: string | null;
+  scheduled_date: string;
+  scheduled_time: string;
+  meeting_link: string | null;
+}
+
+export interface CategorizedMeetings {
+  todays: Meeting[];
+  upcoming: Meeting[];
+  past: Meeting[];
+}
+
+export interface MeetingsApiResponse extends CategorizedMeetings {}
